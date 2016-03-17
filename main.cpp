3fd9f2efc3b4fff6f6f984d87b388db43372c58f
HankLyu <>
2016-03-17 15:07:25 +0800

    Change ReadMe to add thie projcet purpose.

--- main.cpp
+++ main.cpp
@@ -58,7 +58,7 @@ int main(int argc, char* argv[]) {
 	srand(time(NULL));
 	choiceSeedByRandom(seed);
 
-
+	
 	startTime = time(NULL);
 	greedy(seed, putTimeOfSeed, seedsNumber, timesOfSeedBeEffectedBeforePut, numberOfNodeBeEffectedForEachRound);
 	endTime = time(NULL);
